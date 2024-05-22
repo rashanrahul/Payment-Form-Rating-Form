d81081ddd5951bc08717c5931807a6122646a129
Rashan Rahul <163641337+rashanrahul@users.noreply.github.com>
2024-05-22 17:05:11 +0530

    Add files via upload

---
+++ card.js
@@ -0,0 +1,37 @@
+let tColorA = document.getElementById('tColorA'),
+tColorB =document.getElementById('tColorB'),
+tColorC =document.getElementById('tColorC'),
+iconA =document.querySelector('.fa-credit-card'),
+iconB =document.querySelector('.fa-building-column'),
+iconC =document.querySelector('.fa-wallet'),
+cDetails =document.querySelector('.card-details');
+
+function doFun(){
+    tColorA.style.color ="greenyellow";
+    tColorB.style.color ="#444";
+    tColorC.style.color ="#444";
+    iconA.style.color="greenyellow";
+    iconB.style.color="#aaa";
+    iconC.style.color="#aaa";
+    cDetails.style.display="block";
+
+}
+
+function doFunA(){
+    tColorA.style.color ="#444";
+    tColorB.style.color ="greenyellow";
+    tColorC.style.color ="#444";
+    iconA.style.color="#aaa";
+    iconB.style.color="greenyellow";
+    iconC.style.color="#aaa";
+    cDetails.style.display="none";
+}
+function doFunB(){
+    tColorA.style.color ="#444";
+    tColorB.style.color ="#444";
+    tColorC.style.color ="greenyellow";
+    iconA.style.color="#aaa";
+    iconB.style.color="#aaa";
+    iconC.style.color="greenyellow";
+    cDetails.style.display="none";
+}
